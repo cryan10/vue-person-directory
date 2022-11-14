301777aef596fe2c6db7dba67c8345fb4b257f3c
Carrie <cryan1205@gmail.com>
2022-11-14 15:09:07 -0500

    feat(buttons): moves buttons to own component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 import Directory from './components/Directory.vue';
+import Buttons from './components/Buttons.vue';
 
 export default {
 	name: 'app',
 	components: {
 		Directory,
+		Buttons,
 	},
 	data() {
 		return {
@@ -43,33 +45,9 @@ export default {
 	<div class="member-list" role="presentation">
 		<h1 class="member-list__title">Our Members</h1>
 		<main>
-			<Directory v-bind:users="users" />
+			<Directory :users="users" />
+			<Buttons :currentPage="currentPage" :isFetchingUsers="isFetchingUsers" />
 		</main>
-		<div role="presentation">
-			<button
-				@click="goToPreviousPage"
-				:class="{ 'is--disabled': currentPage === 1 }"
-				:disabled="currentPage === 1 || isFetchingUsers"
-				class="member-list__button"
-			>
-				Back
-			</button>
-			<button
-				@click="goToNextPage"
-				:class="{ 'is--disabled': currentPage === 500 }"
-				:disabled="currentPage === 500 || isFetchingUsers"
-				class="member-list__button m--next"
-			>
-				Next
-			</button>
-		</div>
-		<a
-			:href="`https://randomuser.me/api/?page=${currentPage}&results=10&seed=members&inc=gender,name,location,email,phone,id,picture,dob&format=csv`"
-			download
-			class="member-list__button m--download"
-		>
-			Download CSV
-		</a>
 	</div>
 </template>
 
@@ -82,41 +60,6 @@ export default {
 		font-family: var(--fontStackAlt);
 		color: var(--primary);
 		font-size: 2.4rem;
-	}
-	&__button {
-		background-color: var(--accent);
-		font-family: var(--fontStackAlt);
-		color: var(--black);
-		border: none;
-		border-radius: 4px;
-		box-shadow: 1px 1px 2px var(--box-shadow);
-		padding: 0.8rem 2.4rem;
-		max-width: 18rem;
-		font-size: 1.2rem;
-		&:hover {
-			cursor: pointer;
-		}
-		&.is {
-			&--disabled {
-				opacity: 0.5;
-			}
-		}
-		&.m {
-			&--download {
-				background-color: transparent;
-				color: var(--primary);
-				border: 1px solid var(--primary);
-				align-self: end;
-				margin: 1rem;
-				text-decoration: none;
-				&:hover {
-					background-color: var(--black_s10);
-				}
-			}
-			&--next {
-				margin-left: 2rem;
-			}
-		}
 	}
 }
 </style>
---
+++ src/components/Buttons.vue
@@ -0,0 +1,81 @@
+<script>
+export default {
+	name: 'Buttons',
+	props: ['currentPage', 'isFetchingUsers'],
+};
+</script>
+
+<template>
+	<div role="presentation" class="container">
+		<div class="pagination">
+			<button
+				@click="this.$parent.goToPreviousPage()"
+				:class="{ 'is--disabled': currentPage === 1 }"
+				:disabled="currentPage === 1 || isFetchingUsers"
+				class="button"
+			>
+				Back
+			</button>
+			<button
+				@click="this.$parent.goToNextPage()"
+				:class="{ 'is--disabled': currentPage === 500 }"
+				:disabled="currentPage === 500 || isFetchingUsers"
+				class="button m--next"
+			>
+				Next
+			</button>
+		</div>
+
+		<a
+			:href="`https://randomuser.me/api/?page=${currentPage}&results=10&seed=members&inc=gender,name,location,email,phone,id,picture,dob&format=csv`"
+			download
+			class="button m--download"
+		>
+			Download CSV
+		</a>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.container {
+	display: grid;
+	grid-template-rows: 1fr 1fr;
+	justify-items: center;
+}
+.button {
+	background-color: var(--accent);
+	font-family: var(--fontStackAlt);
+	color: var(--black);
+	border: none;
+	border-radius: 4px;
+	box-shadow: 1px 1px 2px var(--box-shadow);
+	padding: 0.8rem 2.4rem;
+	max-width: 8rem;
+	font-size: 1.2rem;
+	text-align: center;
+	&:hover {
+		cursor: pointer;
+	}
+	&.is {
+		&--disabled {
+			opacity: 0.5;
+		}
+	}
+	&.m {
+		&--download {
+			background-color: transparent;
+			color: var(--primary);
+			border: 1px solid var(--primary);
+			justify-self: end;
+			margin: 1rem;
+			text-decoration: none;
+			&:hover {
+				background-color: var(--black_s10);
+			}
+		}
+		&--next {
+			margin-left: 1.5rem;
+		}
+	}
+}
+</style>
